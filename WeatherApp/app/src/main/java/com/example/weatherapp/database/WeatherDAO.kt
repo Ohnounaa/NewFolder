d946652a8bdbfc2f2c654b0ef677b964f0ef65b1
AlizaO <aliza.ohouna1@gmail.com>
2021-06-06 23:10:22 -0400

    type update to weather dao

--- WeatherApp/app/src/main/java/com/example/weatherapp/database/WeatherDAO.kt
+++ WeatherApp/app/src/main/java/com/example/weatherapp/database/WeatherDAO.kt
@@ -9,7 +9,7 @@ import java.util.*
 @Dao
 interface WeatherDao{
     @Query("SELECT * FROM DailyWeatherInfo")
-    fun getAllWeatherInfo(): LiveData<MutableList<DailyWeatherInfo>>
+    fun getAllWeatherInfo(): LiveData<List<DailyWeatherInfo>>
 
     @Query("SELECT * FROM DailyWeatherInfo WHERE dt=(:dt)")
     fun getSingleDayWeatherInfo(dt:Int): LiveData<DailyWeatherInfo>
